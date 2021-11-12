7d21cb095cb39d67d51a093557af4bdc188f5048
ritmatter <3967620+ritmatter@users.noreply.github.com>
2021-11-12 16:42:56 -0500

    Add skeleton for using Kafka

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module goserver
 go 1.17
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/lib/pq v1.10.4 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.7.0 // indirect
 )
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
   "flag"
   "fmt"
   _ "github.com/lib/pq"
+  "github.com/confluentinc/confluent-kafka-go/kafka"
   "github.com/gorilla/websocket"
   "net/http"
   "log"
@@ -13,9 +14,113 @@ import (
 
 var db *sql.DB
 var enableDatabase bool
+var enableKafka bool
 var upgrader = websocket.Upgrader {
   ReadBufferSize:  1024,
   WriteBufferSize: 1024,
+}
+
+var consumer *kafka.Consumer
+var producer *kafka.Producer
+var deliveryChan chan kafka.Event
+
+// Indefinitely polls a consumer.
+func PollConsumer(c *kafka.Consumer) {
+  run := true
+  for run == true {
+    ev := c.Poll(1000)
+    switch e := ev.(type) {
+      case *kafka.Message:
+          fmt.Printf("%% Message on %s:\n%s\n",
+              e.TopicPartition, string(e.Value))
+      case kafka.PartitionEOF:
+          fmt.Printf("%% Reached %v\n", e)
+      case kafka.Error:
+          fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+          run = false
+      default:
+          fmt.Printf("Ignored %v\n", e)
+    }
+  }
+}
+
+// Creates a kafka consumer and subscribes to topics.
+func CreateConsumer(topics *[]string, brokerServer string) (*kafka.Consumer, error) {
+  consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+     "bootstrap.servers":    brokerServer,
+     "group.id":             "foo",
+     "auto.offset.reset":    "smallest"})
+  if err != nil {
+    return consumer, err
+  }
+
+  err = consumer.SubscribeTopics(*topics, nil)
+  return consumer, err
+}
+
+// Creates a kafka producer.
+func CreateProducer(brokerServer string) (*kafka.Producer, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+  fmt.Println("Got hostname: " + hostname)
+
+  return kafka.NewProducer(&kafka.ConfigMap{
+    "bootstrap.servers": brokerServer,
+    "client.id": hostname,
+    "acks": "all"})
+}
+
+// Producers a message with the given producer.
+func ProduceMessage(p *kafka.Producer, deliveryChan chan kafka.Event, value string, topic string) error {
+  fmt.Println("Producing message: " + value + " for topic: " + topic)
+  return p.Produce(&kafka.Message{
+      TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+      Value: []byte(value)},
+      deliveryChan,
+  )
+}
+
+// Handle a produce event for a given producer.
+// Create a go routine with this func for async writes.
+func HandleProduceEvent(p *kafka.Producer) {
+  for e := range p.Events() {
+      switch ev := e.(type) {
+      case *kafka.Message:
+          if ev.TopicPartition.Error != nil {
+              fmt.Printf("Failed to deliver message: %v\n", ev.TopicPartition)
+          } else {
+              fmt.Printf("Successfully produced record to topic %s partition [%d] @ offset %v\n",
+                  *ev.TopicPartition.Topic, ev.TopicPartition.Partition, ev.TopicPartition.Offset)
+          }
+      }
+  }
+}
+
+// Produces a message synchronously. Useful for testing.
+func SynchronousProduce(p *kafka.Producer, topic string, value string) {
+  delivery_chan := make(chan kafka.Event, 10000)
+  err := p.Produce(&kafka.Message{
+      TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+      Value: []byte(value)},
+      delivery_chan)
+  if err != nil {
+   panic(err)
+  }
+
+  e := <-delivery_chan
+  m := e.(*kafka.Message)
+
+  fmt.Println("Examining result of delivery")
+  if m.TopicPartition.Error != nil {
+    fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+  } else {
+    fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+               *m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+  }
+  close(delivery_chan)
 }
 
 // Reader that receives web socket messages
@@ -91,7 +196,38 @@ func ShowWords(w http.ResponseWriter, req *http.Request) {
 func main() {
   fmt.Println("Starting up!")
   enableDatabasePtr := flag.Bool("enableDatabase", false, "Whether to use the database.")
+  enableKafkaPtr := flag.Bool("enableKafka", false, "Whether to use Kafka messaging.")
+  kafkaBrokerServerPtr := flag.String("kafkaBrokerServer", "", "The kafka broker information.")
+  kafkaTopicPtr := flag.String("kafkaTopic", "AwsChatTopic", "The kafka topic")
   flag.Parse()
+
+  enableKafka = *enableKafkaPtr
+  if (enableKafka) {
+    topics := []string{*kafkaTopicPtr}
+    consumerPtr, err := CreateConsumer(&topics, *kafkaBrokerServerPtr)
+    if err != nil {
+      panic(err)
+    }
+    consumer = consumerPtr
+    go PollConsumer(consumer)
+
+    producerPtr, err := CreateProducer(*kafkaBrokerServerPtr)
+    if err != nil {
+      panic(err)
+    }
+    producer = producerPtr
+    deliveryChan := make(chan kafka.Event, 10000)
+    go HandleProduceEvent(producer)
+
+    SynchronousProduce(producer, topics[0], "Sync pub")
+    err = ProduceMessage(producer, deliveryChan, "Hello world!", topics[0])
+    if err != nil {
+      panic(err)
+    } else {
+      fmt.Println("Successfully produced message.")
+    }
+    fmt.Println("Initialized Kafka producer and consumer.")
+  }
 
   enableDatabase = *enableDatabasePtr
   if (enableDatabase) {
