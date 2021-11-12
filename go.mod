d50b3e1c9a43744a511620e77ea76736059535c8
ritmatter <3967620+ritmatter@users.noreply.github.com>
2021-11-11 23:35:57 -0500

    Add basic websocket functionality

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module goserver
 
 go 1.17
 
-require github.com/lib/pq v1.10.4 // indirect
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
--- server.go
+++ server.go
@@ -1,8 +1,10 @@
 package main
 import (
   "database/sql"
+  "flag"
   "fmt"
   _ "github.com/lib/pq"
+  "github.com/gorilla/websocket"
   "net/http"
   "log"
   "os"
@@ -10,63 +12,123 @@ import (
 )
 
 var db *sql.DB
+var enableDatabase bool
+var upgrader = websocket.Upgrader {
+  ReadBufferSize:  1024,
+  WriteBufferSize: 1024,
+}
+
+// Reader that receives web socket messages
+func reader(conn *websocket.Conn) {
+  for {
+    // Read message.
+    messageType, p, err := conn.ReadMessage()
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    // Print message.
+    fmt.Println("Received message: `" + string(p) + "`")
+
+    response := "Ack: " + string(p)
+    if err := conn.WriteMessage(messageType, []byte(response)); err != nil {
+        log.Println(err)
+        return
+    }
+  }
+}
+
+func OpenWebsocket(w http.ResponseWriter, req *http.Request) {
+  // Allow incoming request from another domain to connect (prevent CORS error).
+  upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+  // Upgrade the connection to a web socket.
+  ws, err := upgrader.Upgrade(w, req, nil)
+  if err != nil {
+      log.Println(err)
+  }
+
+  // Log statement to show that client connected.
+  log.Println("Client Connected")
+
+  // Write a hello message to the client.
+  err = ws.WriteMessage(1, []byte("Greetings client."))
+  if err != nil {
+      log.Println(err)
+  }
+
+  // Permanently read from this websocket.
+  reader(ws)
+}
 
 func ShowWords(w http.ResponseWriter, req *http.Request) {
   fmt.Println("Inside ShowWords handler")
 
-  sqlStatement := "SELECT * From words"
-  rows, err := db.Query(sqlStatement)
-  if err != nil {
-    panic(err)
-  }
-  defer rows.Close()
+  if (enableDatabase) {
+    sqlStatement := "SELECT * From words"
+    rows, err := db.Query(sqlStatement)
+    if err != nil {
+      panic(err)
+    }
+    defer rows.Close()
 
 
-  words := make([]string, 0)
-  for rows.Next() {
-    var word string
-		if err := rows.Scan(&word); err != nil {
-			log.Fatal(err)
-		}
-		words = append(words, word)
+    words := make([]string, 0)
+    for rows.Next() {
+      var word string
+      if err := rows.Scan(&word); err != nil {
+        log.Fatal(err)
+      }
+      words = append(words, word)
+    }
+    fmt.Fprint(w, words)
+  } else {
+    fmt.Fprint(w, "Database disabled")
   }
-  fmt.Fprint(w, words)
 }
 
 func main() {
   fmt.Println("Starting up!")
-  fmt.Println(os.Args)
+  enableDatabasePtr := flag.Bool("enableDatabase", false, "Whether to use the database.")
+  flag.Parse()
 
-  host := os.Args[1]
-  port, porterr := strconv.Atoi(os.Args[2])
-  if porterr != nil {
-    panic(porterr)
+  enableDatabase = *enableDatabasePtr
+  if (enableDatabase) {
+    host := os.Args[1]
+    port, err := strconv.Atoi(os.Args[2])
+    if err != nil {
+      panic(err)
+    }
+
+    user := os.Args[3]
+    password := os.Args[4]
+    dbname := os.Args[5]
+
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+      "password=%s dbname=%s sslmode=disable",
+      host, port, user, password, dbname)
+    fmt.Println(psqlInfo)
+
+    database, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+      panic(err)
+    }
+    defer database.Close()
+    db = database
+
+    err = db.Ping()
+    if err != nil {
+      panic(err)
+    }
+    fmt.Println("Successfully connected to database!")
+  } else {
+   fmt.Println("Skipping database connection")
   }
 
-  user := os.Args[3]
-  password := os.Args[4]
-  dbname := os.Args[5]
-
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
-  fmt.Println(psqlInfo)
-
-  database, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer database.Close()
-  db = database
-
-  err = db.Ping()
-  if err != nil {
-    panic(err)
-  }
-
-  fmt.Println("Successfully connected!")
-  http.HandleFunc("/",ShowWords)
-  err = http.ListenAndServe(":3000", nil)
+  http.HandleFunc("/", ShowWords)
+  http.HandleFunc("/ws", OpenWebsocket)
+  err := http.ListenAndServe(":3000", nil)
   fmt.Println("Serving on port 3000")
   if err != nil {
     log.Fatal("ListenAndServe: ", err.Error())
